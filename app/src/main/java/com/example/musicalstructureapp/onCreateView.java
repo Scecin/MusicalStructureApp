103fa2d6eeb278bddef9e6f87c587b6fc75d34ae
SILVIA-CECIN <silvia.cecin@hotmail.com>
2021-02-21 14:17:10 +0000

    New Music App

---
+++ app/src/main/java/com/example/musicalstructureapp/onCreateView.java
@@ -0,0 +1,10 @@
+package com.example.musicalstructureapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public interface onCreateView {
+    View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState);
+}
